a03e2c67a115e32a1d8301617061333e4bdda98f
TBXark <tbxark@outlook.com>
2022-03-31 13:53:53 +0800

    update example

--- router.js
+++ router.js
@@ -1,11 +1,14 @@
 import Router from "@koa/router";
-import { mapBody } from "./utils.js";
+import { mapBody, disableLog } from "./utils.js";
 
 export const router = new Router();
 
 // Add Code Here
 
 // Example
+// 1.disable some api log 
+router.get('/', disableLog())
+// 2. change response body
 router.get( "/users/:name", mapBody((res, ctx) => {
     const { params, query } = ctx;
     console.log(params);
