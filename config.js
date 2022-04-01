83d7255d8878cefb977d9c7643e6e15de2af8ebe
TBXark <tbxark@outlook.com>
2022-04-01 16:54:32 +0800

    fix typo

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ export const SERVER_PORT = args.port || 3000;
 export const SHOW_REQUEST_HEADER = false;
 // 打印请求体
 export const SHOW_REQUEST_BODY = true;
-// 答应响应体
+// 打印响应体
 export const SHOW_RESPONSE_BODY = true;
 // 响应体JSON格式化输出
 export const PRINT_RESPONSE_JSON_PRETTY = false;
