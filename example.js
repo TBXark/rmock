7af62fc8e1242b336ab21b5cdeaca0b1fe2db063
TBXark <tbxark@outlook.com>
2022-04-04 00:55:10 +0800

    fix router.mjs

--- example.js
+++ example.js
@@ -1,45 +1,44 @@
 // Import core modules using require
 //
-const os = require('os')
-console.log(os.arch())
+const os = require('os');
+console.log(os.arch());
 
 async function register(router, utils, importModule) {
-
   // Commonly used functions are injected by default
   //
   //
-  const { mapJSON, customMapper, redirect } = utils;
+  const {mapJSON, customMapper, redirect} = utils; // eslint-disable-line
 
   // Import custom modules using importModule
   //
   // get default export manually
-  const { default: fetch } = await importModule('node-fetch');
+  const {default: fetch} = await importModule('node-fetch');
 
   // Example
   //
   //
-  
+
   // 1. disable some api log
-  router.get('/', mapJSON(null, { canLog: false }))
+  router.get('/', mapJSON(null, {canLog: false}));
 
   // 2. change response body
   router.get('/status', mapJSON((res, ctx) => {
     return {
       ...res,
-      inject: 'Hello World!!!'
+      inject: 'Hello World!!!',
     };
-  }))
+  }));
 
   // 3. async mapper
   router.get('/users/:id', mapJSON(async (res, ctx) => {
-    const { id } = ctx.params;
-    const repos = await fetch(`https://api.github.com/users/${id}/repos`).then(res => res.json());
-    console.log(repos)
+    const {id} = ctx.params;
+    const repos = await fetch(`https://api.github.com/users/${id}/repos`).then((res) => res.json());
+    console.log(repos);
     return {
       ...res,
-      repos
+      repos,
     };
-  }))
+  }));
 }
 
 // use commonjs export
